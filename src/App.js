93efd7516aa7496a9df229d05d7ca1a203b01cb1
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-06 00:16:48 +0400

    add route to appjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import SubjectButtons from './SubjectButtons';
+import SubjectTable from './SubjectTable';
+import StudentTable from './components/StudentTable';
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <div>
+        <SubjectButtons />
+        <Switch>
+          <Route path="/subject/georgian" component={() => <SubjectTable subject="Georgian" />} />
+          <Route path="/subject/math" component={() => <SubjectTable subject="Mathematics" />} />
+          <Route path="/subject/english" component={() => <SubjectTable subject="English" />} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
