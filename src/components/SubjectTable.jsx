51e74e4e0f51eb160b803c4216f643c6088566c1
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-06 00:23:10 +0400

    creating way to table

---
+++ src/components/SubjectTable.jsx
@@ -0,0 +1,15 @@
+// SubjectTable.js
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function SubjectTable() {
+  const { subject } = useParams();
+
+  return (
+    <div id="tableContainer">
+
+    </div>
+  );
+}
+
+export default SubjectTable;
