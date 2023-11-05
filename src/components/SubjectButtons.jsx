70aedfe2d9a0324fc594c11fa4a216bf01a7e503
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-06 00:19:20 +0400

    subject button link

---
+++ src/components/SubjectButtons.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function SubjectButtons() {
+  return (
+    <div id="buttons">
+      <Link to="/subject/georgian">Georgian</Link>
+      <Link to="/subject/math">Mathematics</Link>
+      <Link to="/subject/english">English</Link>
+    </div>
+  );
+}
+
+export default SubjectButtons;
