4cbf1f0fbe1133837d9a050c8cf86494b68e1f4b
მაცი <93665709+maci03@users.noreply.github.com>
2023-11-06 21:05:25 +0400

    route to weeks

--- src/components/WeekFilterButtons.js
+++ src/components/WeekFilterButtons.js
@@ -1,2 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+
+function WeekFilterButtons() {
+    return (
+      <div id="week-filter-buttons">
+        <Link to="/week/first">First Week</Link>
+        <Link to="/week/second">Second Week</Link>
+        <Link to="/week/third">Third Week</Link>
+      </div>
+    );
+  }
+  
+  export default WeekFilterButtons;
